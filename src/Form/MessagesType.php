8b1e148a8e8e91768b726427e9bfeea4f76b069c
SkanderBaatout <skanderbaatout2@gmail.com>
2021-12-23 16:27:46 +0100

    makers

--- src/Form/MessagesType.php
+++ src/Form/MessagesType.php
@@ -4,7 +4,7 @@ namespace App\Form;
 
 use App\Entity\Messages;
 use App\Entity\Users;
-use Doctrine\DBAL\Types\TextType;
+use Symfony\Component\Form\Extension\Core\Type\TextType;
 use Symfony\Bridge\Doctrine\Form\Type\EntityType;
 use Symfony\Component\Form\AbstractType;
 use Symfony\Component\Form\Extension\Core\Type\SubmitType;
